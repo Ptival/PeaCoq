820fce9c4ebf503f7857b195251e8c9047c91246
Valentin Robert <vrobert@cs.ucsd.edu>
2014-12-17 17:16:13 -0800

    stress testing

---
+++ web/stress.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function() {
+
+    PT.resetCoq();
+
+    PT.handleKeyboard();
+
+    var scrollbarWidth = 20; // arbitrary
+
+    var pt = new ProofTree(
+        d3.select("body"),
+        $(window).width() - scrollbarWidth,
+        $(window).height(),
+        function(){}
+    );
+
+    makeActive(pt);
+
+    pt.newTheorem(
+        "Theorem stress : False.",
+        function(pt) { return ["pose proof I"]; },
+        function(){}
+    );
+
+    var times = [];
+
+    var n = 0;
+    var bound = 30;
+
+    function proceed() {
+        var before = new Date();
+        pt.click(pt.curNode.children[0],
+                 false,
+                 function() {
+                     var after = new Date();
+                     times.push(after - before);
+                 }
+                );
+        n++;
+        if (n < bound) {
+            window.setTimeout(proceed, 1000);
+        } else {
+            console.log(times);
+        }
+    }
+
+    proceed();
+
+});
