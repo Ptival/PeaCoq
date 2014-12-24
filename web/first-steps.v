0f4eb57a73262cb9c139253b4bbdf4a389245958
Valentin Robert <vrobert@cs.ucsd.edu>
2014-12-24 00:37:26 -0800

    changed test

--- web/first-steps.v
+++ web/first-steps.v
@@ -1,11 +1,7 @@
 Require Import Unicode.Utf8.
 
 (*
-  Welcome to the PeaCoq frontend to the Coq Proof Assistant.
- *)
-
-(*
-Here is some code:
+  Welcome. Here is some code:
  *)
 
 Inductive bool : Type :=
@@ -22,16 +18,14 @@ Proof.
   { right. reflexivity. }
 Qed.
 
-(* Here is another theorem: *)
-
-Theorem bools_are_not_true_and_false : ∀ b, ~ (b = true /\ b = false).
+Theorem bools_are_not_true_and_false :
+  ∀ b, ¬ (b = true ∧ b = false).
 Proof.
   intro. intro. destruct H. destruct b. inversion H0. inversion H.
 Qed.
 
 (* Yet another theorem: *)
 
-Theorem bools_are_not_true_and_false : ∀ b, ~ (b = true /\ b = false).
+Theorem t : False.
 Proof.
-  intro. intro. destruct H. destruct b. inversion H0. inversion H.
-Qed.
+Admitted.
