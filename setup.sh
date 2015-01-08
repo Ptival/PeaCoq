6325c7b9d6ba86bc58d7da0775dee2977d95ed88
Valentin Robert <vrobert@cs.ucsd.edu>
2015-01-07 16:54:04 -0800

    better setup, should be different but no time to do that

--- setup.sh
+++ setup.sh
@@ -1,62 +1,56 @@
 #!/bin/sh -e
 
+MKDIR="mkdir -p"
+GET="wget -O - "
+LN="ln -sf"
+
 cd web/
 
-if [ ! -f bootstrap.js ]; then
-    mkdir -p bootstrap
-    cd bootstrap/
-    wget https://github.com/twbs/bootstrap/releases/download/v3.3.1/bootstrap-3.3.1-dist.zip
-    unzip bootstrap-3.3.1-dist.zip
-    cd ..
-    ln -s bootstrap/dist/js/bootstrap.min.js bootstrap.js
-    ln -s bootstrap/dist/css/bootstrap.min.css bootstrap.css
-    ln -s bootstrap/dist/css/bootstrap-theme.min.css bootstrap-theme.css
-    ln -s bootstrap/dist/fonts fonts
-fi
+$MKDIR bootstrap
+cd bootstrap/
+$GET https://github.com/twbs/bootstrap/releases/download/v3.3.1/bootstrap-3.3.1-dist.zip > bootstrap-3.3.1-dist.zip
+unzip -o bootstrap-3.3.1-dist.zip
+cd ..
+$LN bootstrap/dist/js/bootstrap.min.js bootstrap.js
+$LN bootstrap/dist/css/bootstrap.min.css bootstrap.css
+$LN bootstrap/dist/css/bootstrap-theme.min.css bootstrap-theme.css
+$LN bootstrap/dist/fonts fonts
 
-if [ ! -f d3.js ]; then
-    mkdir -p d3
-    cd d3
-    wget https://github.com/mbostock/d3/releases/download/v3.4.13/d3.zip
-    unzip d3.zip
-    cd ..
-    ln -s d3/d3.min.js d3.js
-fi
+$MKDIR d3
+cd d3
+$GET https://github.com/mbostock/d3/releases/download/v3.4.13/d3.zip > d3.zip
+unzip -o d3.zip
+cd ..
+$LN d3/d3.min.js d3.js
 
-if [ ! -f jquery.js ]; then
-    mkdir -p jquery
-    cd jquery/
-    wget http://code.jquery.com/jquery-1.11.1.min.js
-    cd ..
-    ln -s jquery/jquery-1.11.1.min.js jquery.js
-fi
+$MKDIR jquery
+cd jquery/
+$GET http://code.jquery.com/jquery-1.11.1.min.js > jquery-1.11.1.min.js
+cd ..
+$LN jquery/jquery-1.11.1.min.js jquery.js
 
-if [ ! -f lodash.js ]; then
-    mkdir -p lodash
-    cd lodash/
-    wget https://raw.github.com/lodash/lodash/2.4.1/dist/lodash.min.js
-    cd ..
-    ln -s lodash/lodash.min.js lodash.js
-fi
+$MKDIR lodash
+cd lodash/
+$GET https://raw.github.com/lodash/lodash/2.4.1/dist/lodash.min.js > lodash.min.js
+cd ..
+$LN lodash/lodash.min.js lodash.js
 
-if [ ! -f rangy-core.js ]; then
-    mkdir -p rangy
-    cd rangy/
-    wget https://cdnjs.cloudflare.com/ajax/libs/rangy/1.2.3/rangy-core.js
-    cd ..
-    ln -s rangy/rangy-core.js rangy-core.js
-fi
+$MKDIR rangy
+cd rangy/
+$GET https://rangy.googlecode.com/svn/trunk/currentrelease/uncompressed/rangy-core.js > rangy-core.js
+$GET https://rangy.googlecode.com/svn/trunk/src/js/modules/rangy-textrange.js > rangy-textrange.js
+cd ..
+$LN rangy/rangy-core.js rangy-core.js
+$LN rangy/rangy-textrange.js rangy-textrange.js
 
-if [ ! -f highlight.js ]; then
-    mkdir -p highlight
-    cd highlight/
-    wget https://raw.githubusercontent.com/highlightjs/cdn-release/master/build/highlight.min.js
-    wget https://raw.githubusercontent.com/highlightjs/cdn-release/master/build/languages/ocaml.min.js
-    wget https://raw.githubusercontent.com/highlightjs/cdn-release/master/build/styles/github.min.css
-    cd ..
-    ln -s highlight/highlight.min.js highlight.js
-    ln -s highlight/ocaml.min.js ocaml.js
-    ln -s highlight/github.min.css highlight.css
-fi
+$MKDIR highlight
+cd highlight/
+$GET https://raw.githubusercontent.com/highlightjs/cdn-release/master/build/highlight.min.js > highlight.min.js
+$GET https://raw.githubusercontent.com/highlightjs/cdn-release/master/build/languages/ocaml.min.js > ocaml.min.js
+$GET https://raw.githubusercontent.com/highlightjs/cdn-release/master/build/styles/github.min.css > github.min.css
+cd ..
+$LN highlight/highlight.min.js highlight.js
+$LN highlight/ocaml.min.js ocaml.js
+$LN highlight/github.min.css highlight.css
 
 cd ..
