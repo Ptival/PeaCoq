7adcd2bdee31ff637313d2639fe9ffa78348f6a2
Valentin Robert <vrobert@cs.ucsd.edu>
2015-01-08 18:04:08 -0800

    updated script, much robust

--- cse505.sh
+++ cse505.sh
@@ -8,17 +8,16 @@ PEACOQCONFIG=".PeaCoqConfig.hs"
 
 export PATH="~ztatlock/.cabal/bin:~ztatlock/local/bin:$PATH"
 
-mkdir -p "${CONFIGPATH}"
-mkdir -p "${LOGPATH}"
-
 case "$1" in
     "nolog")
         echo "PeaCoqConfig { configUserId  = Nothing, configLogPath = \"${LOGPATH}\" }" > "${CONFIGPATH}/${PEACOQCONFIG}"
         ;;
     *)
-        STUDYID=`echo -n ${USERNAME} | md5sum`
-        echo "PeaCoqConfig { configUserId  = Just \"$STUDYID\", configLogPath = \"$LOGPATH\" }" > "${CONFIGPATH}/${PEACOQCONFIG}"
+        STUDYID=$(echo -n "${USERNAME}" | md5sum | awk '{print $1}')
+        echo "PeaCoqConfig { configUserId  = Just \"${STUDYID}\", configLogPath = \"${LOGPATH}\" }" > "${CONFIGPATH}/${PEACOQCONFIG}"
         ;;
 esac
 
+cd "${ZACHOME}/PeaCoq/"
 
+peacoq &
