945f33ba715419e536fa085fcf1441a081d8e352
Zach Tatlock <ztatlock@cs.washington.edu>
2015-01-29 23:18:06 -0800

    wat

--- analysis/log2csv.py
+++ analysis/log2csv.py
@@ -1,8 +1,6 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
-import csv
-import os
-import re
+import os, io, re, csv
 
 from os.path import basename, splitext
 from glob import glob
@@ -41,62 +39,63 @@ if __name__ == '__main__':
         if 'access' in file: continue
         (root, ext) = splitext(file)
         userFilename = basename(file).split('-')[0]
-        with open(root + '.csv', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, dialect='unix')
-            filetext = open(file).read()
-            items = rItem.split(filetext)
-            for item in items:
-                if item == "":
-                    continue
-                # there has got to be a better way to do this :(
+        with io.open(root + '.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            with open(file) as handle:
+              filetext = h.read()
+              items = rItem.split(filetext)
+              for item in items:
+                  if item == "":
+                      continue
+                  # there has got to be a better way to do this :(
 
-                m = r4.match(item)
-                if m:
-                    args = list(map(lambda x: int(x), (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6))))
-                    serverdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
-                    user = m.group(7)
-                    args = list(map(lambda x: int(x), (m.group(10), m.group(8), m.group(9), adjust(m.group(11), m.group(14)), m.group(12), m.group(13))))
-                    clientdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
-                    command = m.group(15)
-                    payload = m.group(16)
-                    print(serverdate, clientdate, command)
-                    writer.writerow([user, serverdate, clientdate, command, payload])
-                    continue
+                  m = r4.match(item)
+                  if m:
+                      args = [ int(m.group(i)) for i in range(1, 7) ]
+                      serverdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
+                      user = m.group(7)
+                      args = list(map(lambda x: int(x), (m.group(10), m.group(8), m.group(9), adjust(m.group(11), m.group(14)), m.group(12), m.group(13))))
+                      clientdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
+                      command = m.group(15)
+                      payload = m.group(16)
+                      print(serverdate, clientdate, command)
+                      writer.writerow([user, serverdate, clientdate, command, payload])
+                      continue
 
-                m = r3.match(item)
-                if m:
-                    args = list(map(lambda x: int(x), (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6))))
-                    serverdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
-                    user = m.group(7)
-                    clientdate = ""
-                    command = m.group(8)
-                    payload = m.group(9)
-                    print(serverdate, clientdate, command)
-                    writer.writerow([userFilename, serverdate, clientdate, command, payload])
-                    continue
+                  m = r3.match(item)
+                  if m:
+                      args = list(map(lambda x: int(x), (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6))))
+                      serverdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
+                      user = m.group(7)
+                      clientdate = ""
+                      command = m.group(8)
+                      payload = m.group(9)
+                      print(serverdate, clientdate, command)
+                      writer.writerow([userFilename, serverdate, clientdate, command, payload])
+                      continue
 
-                m = r2.match(item)
-                if m:
-                    args = list(map(lambda x: int(x), (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6))))
-                    serverdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
-                    args = list(map(lambda x: int(x), (m.group(9), m.group(7), m.group(8), adjust(m.group(10), m.group(13)), m.group(11), m.group(12))))
-                    clientdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
-                    command = m.group(14)
-                    payload = m.group(15)
-                    print(serverdate, clientdate, command)
-                    writer.writerow([userFilename, serverdate, clientdate, command, payload])
-                    continue
+                  m = r2.match(item)
+                  if m:
+                      args = list(map(lambda x: int(x), (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6))))
+                      serverdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
+                      args = list(map(lambda x: int(x), (m.group(9), m.group(7), m.group(8), adjust(m.group(10), m.group(13)), m.group(11), m.group(12))))
+                      clientdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
+                      command = m.group(14)
+                      payload = m.group(15)
+                      print(serverdate, clientdate, command)
+                      writer.writerow([userFilename, serverdate, clientdate, command, payload])
+                      continue
 
-                m = r1.match(item)
-                if m:
-                    args = list(map(lambda x: int(x), (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6))))
-                    serverdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
-                    clientdate = ""
-                    command = m.group(7)
-                    payload = m.group(8)
-                    print(serverdate, clientdate, command)
-                    writer.writerow([userFilename, serverdate, clientdate, command, payload])
-                    continue
+                  m = r1.match(item)
+                  if m:
+                      args = list(map(lambda x: int(x), (m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6))))
+                      serverdate = "{:04d}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}".format(*args)
+                      clientdate = ""
+                      command = m.group(7)
+                      payload = m.group(8)
+                      print(serverdate, clientdate, command)
+                      writer.writerow([userFilename, serverdate, clientdate, command, payload])
+                      continue
 
-                print('FAILED', item)
-                exit(0)
+                  print('FAILED', item)
+                  exit(0)
