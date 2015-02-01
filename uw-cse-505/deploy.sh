f250f9a7779aec988d9a7c61cdbccf8ff065ff7f
Zach Tatlock <ztatlock@cs.washington.edu>
2015-02-01 00:20:53 -0800

    fix deploy

--- uw-cse-505/deploy.sh
+++ uw-cse-505/deploy.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 ssh attu.cs.washington.edu     "\$HOME/PeaCoq/uw-cse-505/update.sh"
 ssh tricycle.cs.washington.edu "\$HOME/PeaCoq/uw-cse-505/update.sh"
 
-ssh tricycle.cs.washington.edu "\$HOME/PeaCoq/start-peacoq.sh bboston7 67960"
-ssh tricycle.cs.washington.edu "\$HOME/PeaCoq/start-peacoq.sh georgem6 58870"
-ssh tricycle.cs.washington.edu "\$HOME/PeaCoq/start-peacoq.sh asnchstr 67780"
-ssh tricycle.cs.washington.edu "\$HOME/PeaCoq/start-peacoq.sh kimyen   58690"
+ssh tricycle.cs.washington.edu "\$HOME/PeaCoq/stop-peacoq.sh || true"
+
+ssh tricycle.cs.washington.edu <<EOF
+nohup \$HOME/PeaCoq/start-peacoq.sh bboston7 60160 &>/dev/null &
+EOF
+ssh tricycle.cs.washington.edu <<EOF
+nohup \$HOME/PeaCoq/start-peacoq.sh georgem6 58870 &>/dev/null &
+EOF
+ssh tricycle.cs.washington.edu <<EOF
+nohup \$HOME/PeaCoq/start-peacoq.sh asnchstr 60180 &>/dev/null &
+EOF
+ssh tricycle.cs.washington.edu <<EOF
+nohup \$HOME/PeaCoq/start-peacoq.sh kimyen 58690 &>/dev/null &
+EOF
