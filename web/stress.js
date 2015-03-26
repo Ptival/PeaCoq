e41db887299d34033815e8f6d6705322be49862f
Valentin Robert <vrobert@cs.ucsd.edu>
2015-03-25 22:13:49 -0700

    new stress test, probably not the best

--- web/stress.js
+++ web/stress.js
@@ -1,50 +1,69 @@
-
-$(document).ready(function() {
-
-    PT.resetCoq();
-
-    PT.handleKeyboard();
-
-    var scrollbarWidth = 20; // arbitrary
-
-    var pt = new ProofTree(
-        d3.select("body"),
-        $(window).width() - scrollbarWidth,
-        $(window).height(),
-        function(){}
+function stress() {
+    // fake loading some file
+    onLoad(
+        " \
+Ltac ackerman m n := \
+match m with \
+| O => constr:(S n) \
+| S ?m' => \
+match n with \
+| O => \
+let res := ackerman m' 1 in \
+constr:(res) \
+| S ?n' => \
+let tmp1 := ackerman m n' in \
+let tmp2 := ackerman m' tmp1 in \
+constr:(tmp2) \
+end \
+end. \
+Theorem t (n:nat) (m:nat) (H: n + m = 42) : False. \
+Proof. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+let res := ackerman 3 5 in pose res. \
+Admitted. \
+"
     );
-
-    makeActive(pt);
 
     var times = [];
 
-    var n = 0;
-
-    var bound = 30;
-
-    function proceed() {
-        var before = new Date();
-        pt.click(
-            pt.curNode.children[0],
-            false,
-            function() {
+    function waitThenAct() {
+        return delayPromise(1000)()
+            .then(function() {
                 var after = new Date();
-                times.push(after - before);
-                n++;
-                if (n < bound) {
-                    window.setTimeout(proceed, 1000);
-                } else {
-                    console.log(times);
-                }
-            }
-        );
+                var diff = after - before;
+                console.log(before, after, diff);
+                times.push(diff);
+                return onCtrlDown(true);
+            })
+        ;
     }
 
-    pt.newTheorem(
-        "Theorem stress : False.",
-        function(pt) { return ["pose proof I"]; },
-        function(){},
-        function(){ console.log("proceeding"); proceed(); }
-    );
+    // could have been a _.reduce if I cared...
+    var before = new Date();
+    onCtrlDown(true)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(waitThenAct)
+        .then(function() {
+            console.log(times);
+        })
+    ;
 
-});
+}
