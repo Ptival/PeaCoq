ab20ab0b62e25eeb6ffc848a5b97b65d3e712b60
Valentin Robert <vrobert@cs.ucsd.edu>
2015-05-19 13:39:28 -0700

    removing study-specific things from master branch

--- web/lecture.js
+++ web/lecture.js
@@ -202,48 +202,48 @@ $(document).ready(function() {
     //     })
     // ;
 
-    $("<button>", {
-        "class": "btn btn-primary",
-        "id": "ask-for-help-button",
-        "html": $("<span>")
-            .append(mkGlyph("user"))
-            .append(nbsp + nbsp + "Ask for help"),
-    })
-        .appendTo(buttonGroup)
-        .on("click", function() {
-            asyncLog("NEEDHELP");
-            $("#ask-for-help-button").css("display", "none");
-            $("#asked-for-help-button").css("display", "");
-        })
-    ;
+    // $("<button>", {
+    //     "class": "btn btn-primary",
+    //     "id": "ask-for-help-button",
+    //     "html": $("<span>")
+    //         .append(mkGlyph("user"))
+    //         .append(nbsp + nbsp + "Ask for help"),
+    // })
+    //     .appendTo(buttonGroup)
+    //     .on("click", function() {
+    //         asyncLog("NEEDHELP");
+    //         $("#ask-for-help-button").css("display", "none");
+    //         $("#asked-for-help-button").css("display", "");
+    //     })
+    // ;
 
-    $("<button>", {
-        "class": "btn btn-primary",
-        "id": "asked-for-help-button",
-        "html": $("<span>")
-            .append(mkGlyph("user"))
-            .append(nbsp + nbsp + "You asked for help"),
-    })
-        .appendTo(buttonGroup)
-        .css("display", "none")
-    ;
+    // $("<button>", {
+    //     "class": "btn btn-primary",
+    //     "id": "asked-for-help-button",
+    //     "html": $("<span>")
+    //         .append(mkGlyph("user"))
+    //         .append(nbsp + nbsp + "You asked for help"),
+    // })
+    //     .appendTo(buttonGroup)
+    //     .css("display", "none")
+    // ;
 
-    $("<button>", {
-        "class": "btn btn-danger",
-        "id": "help-in-progress-button",
-        "html": $("<span>")
-            .append(mkGlyph("user"))
-            .append(nbsp + nbsp + "Help in progress, click here when done!"),
-    })
-        .appendTo(buttonGroup)
-        .css("display", "none")
-        .on("click", function() {
-            asyncLog("HELPEND");
-            $(this).css("display", "none");
-            $("#ask-for-help-button").css("display", "");
-            $("#asked-for-help-button").css("display", "none");
-        })
-    ;
+    // $("<button>", {
+    //     "class": "btn btn-danger",
+    //     "id": "help-in-progress-button",
+    //     "html": $("<span>")
+    //         .append(mkGlyph("user"))
+    //         .append(nbsp + nbsp + "Help in progress, click here when done!"),
+    // })
+    //     .appendTo(buttonGroup)
+    //     .css("display", "none")
+    //     .on("click", function() {
+    //         asyncLog("HELPEND");
+    //         $(this).css("display", "none");
+    //         $("#ask-for-help-button").css("display", "");
+    //         $("#asked-for-help-button").css("display", "none");
+    //     })
+    // ;
 
     $("#feedback").on('shown.bs.modal', function () {
         $("#feedback-form").focus();
@@ -952,15 +952,15 @@ function lectureTactics(pt) {
             (pt.goalIsReflexive() ? ["reflexivity"] : [])
                 .concat([
                     "discriminate",
-                    //"assumption",
-                    //"eassumption",
+                    "assumption",
+                    "eassumption",
                 ])
         ),
 
-        // makeGroup(
-        //     "autos",
-        //     ["auto", "eauto"]
-        // ),
+        makeGroup(
+            "autos",
+            ["auto", "eauto"]
+        ),
 
         makeGroup(
             "introductions",
@@ -971,8 +971,8 @@ function lectureTactics(pt) {
             "break_if, f_equal, subst",
             [
                 "break_if",
-                //"f_equal",
-                //"repeat f_equal",
+                "f_equal",
+                "repeat f_equal",
                 "subst"
             ]
         ),
@@ -990,20 +990,20 @@ function lectureTactics(pt) {
             "constructors",
             (pt.goalIsDisjunction() ? ["left", "right"] : [])
                 .concat(pt.goalIsConjunction() ? ["split"] : [])
-                // .concat([
-                //     "constructor",
-                //     "econstructor",
-                //     "eexists",
-                // ])
+                .concat([
+                    "constructor",
+                    "econstructor",
+                    "eexists",
+                ])
         ),
 
-        // makeGroup(
-        //     "destructors",
-        //     _(curHyps)
-        //         .filter(function(h) { return isLowerCase(h[0]); })
-        //         .map(function(h) { return "destruct " + h; })
-        //         .value()
-        // ),
+        makeGroup(
+            "destructors",
+            _(curHyps)
+                .filter(function(h) { return isLowerCase(h[0]); })
+                .map(function(h) { return "destruct " + h; })
+                .value()
+        ),
 
         makeGroup(
             "inductions",
@@ -1015,36 +1015,29 @@ function lectureTactics(pt) {
                 .value()
         ),
 
-        // makeGroup(
-        //     "inversions",
-        //     _(curHyps).map(function(h) { return "inversion " + h; }).value()
-        // ),
+        makeGroup(
+            "inversions",
+            _(curHyps).map(function(h) { return "inversion " + h; }).value()
+        ),
 
-        // makeGroup(
-        //     "solvers",
-        //     ["congruence", "omega", "firstorder"]
-        // ),
+        makeGroup(
+            "solvers",
+            ["congruence", "omega", "firstorder"]
+        ),
 
         makeGroup(
             "applications",
             _(curNames).map(function(n) { return "apply " + n; }).value()
-                // .concat(
-                //     _(curNames).map(function(n) { return "eapply " + n; }).value()
-                // )
+                .concat(
+                    _(curNames).map(function(n) { return "eapply " + n; }).value()
+                )
         ),
 
         makeGroup(
             "rewrites",
             _(curNames)
                 .map(function(n) {
-                    switch (n) {
-                    case "concat_nil_left":
-                    case "concat_nil_right":
-                    case "rev_involutive":
-                        return ["rewrite -> " + n];
-                    default:
-                        return ["rewrite -> " + n, "rewrite <- " + n];
-                    };
+                    return ["rewrite -> " + n, "rewrite <- " + n];
                 })
                 .flatten(true).value()
         ),
@@ -1057,7 +1050,7 @@ function lectureTactics(pt) {
                         if (h === n) { return []; }
                         return ([
                             "apply " + n + " in " + h,
-                            //"eapply " + n + " in " + h,
+                            "eapply " + n + " in " + h,
                         ]);
                     })
                     .flatten(true).value();
@@ -1070,29 +1063,20 @@ function lectureTactics(pt) {
                 return _(curHyps)
                     .map(function(h) {
                         if (h === n) { return []; }
-                        switch (n) {
-                        case "concat_nil_left":
-                        case "concat_nil_right":
-                        case "rev_involutive":
-                            return ([
-                                ("rewrite -> " + n + " in " + h),
-                            ]);
-                        default:
-                            return ([
-                                ("rewrite -> " + n + " in " + h),
-                                ("rewrite <- " + n + " in " + h)
-                            ]);
-                        };
+                        return ([
+                            ("rewrite -> " + n + " in " + h),
+                            ("rewrite <- " + n + " in " + h)
+                        ]);
                     })
                     .flatten(true).value()
                 ;
             }).flatten(true).value()
         ),
 
-        // makeGroup(
-        //     "reverts",
-        //     _(curHyps).map(function(h) { return "revert " + h; }).value()
-        // ),
+        makeGroup(
+            "reverts",
+            _(curHyps).map(function(h) { return "revert " + h; }).value()
+        ),
 
     ];
 
