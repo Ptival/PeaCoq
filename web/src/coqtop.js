cae26d3fdf8dc724a9adae9f6a73258a93759947
Valentin Robert <vrobert@cs.ucsd.edu>
2015-05-19 13:05:48 -0700

    flow changes, whatever

--- web/src/coqtop.js
+++ web/src/coqtop.js
@@ -11,9 +11,9 @@ var ProofTree = require('./prooftree');
 var processingAsync: bool = false;
 
 type PeaCoqRequest = {
-	url: string;
-	query: string;
-	callback: (r: PeaCoqResponse) => void;
+  url: string;
+  query: string;
+  callback: (r: PeaCoqResponse) => void;
 }
 
 var asyncRequests: Array < PeaCoqRequest > = [];
@@ -24,137 +24,137 @@ var asyncRequests: Array < PeaCoqRequest > = [];
 */
 function processAsyncRequests() {
 
-	if (processingAsync) {
-		return;
-	}
+  if (processingAsync) {
+    return;
+  }
 
-	if (_(asyncRequests).isEmpty()) {
-		return;
-	}
+  if (_(asyncRequests).isEmpty()) {
+    return;
+  }
 
-	var request = asyncRequests.shift();
-	var r = request.url;
-	var q = request.query;
-	var h = request.callback;
-	if (r === 'query') {
-		console.log(q);
-	}
-	processingAsync = true;
-	Lecture.onRequestTriggered(r, q);
-	var beforeTime = Date.now();
+  var request = asyncRequests.shift();
+  var r = request.url;
+  var q = request.query;
+  var h = request.callback;
+  if (r === 'query') {
+    console.log(q);
+  }
+  processingAsync = true;
+  Lecture.onRequestTriggered(r, q);
+  var beforeTime = Date.now();
 
-	$.ajax({
-		type: 'POST',
-		url: r,
-		data: {
-			query: q
-		},
-		async: true,
-		error: function() {
-			processingAsync = false;
-			alert("Server did not respond!");
-		},
-		success: function(rawResponse) {
-			var response = new PeaCoqResponse(rawResponse);
+  $.ajax({
+    type: 'POST',
+    url: r,
+    data: {
+      query: q
+    },
+    async: true,
+    error: function() {
+      processingAsync = false;
+      alert("Server did not respond!");
+    },
+    success: function(rawResponse) {
+      var response = new PeaCoqResponse(rawResponse);
 
-			var afterTime = Date.now();
+      var afterTime = Date.now();
 
-			processingAsync = false;
+      processingAsync = false;
 
-			Editor.onResponse(r, q, response);
-			if (Globals.activeProofTree) {
-				Globals.activeProofTree.onResponse(r, q, response);
-			}
+      Editor.onResponse(r, q, response);
+      if (Globals.activeProofTree) {
+        Globals.activeProofTree.onResponse(r, q, response);
+      }
 
-			processAsyncRequests();
+      processAsyncRequests();
 
-			h(response);
+      h(response);
 
-		}
-	});
+    }
+  });
 
 }
 
 function asyncRequest(r: string, q: string): Promise {
-	return new Promise(function(onFulfilled, onRejected) {
-		asyncRequests.push({
-			"url": r,
-			"query": q,
-			"callback": onFulfilled,
-		});
-		processAsyncRequests();
-	});
+  return new Promise(function(onFulfilled, onRejected) {
+    asyncRequests.push({
+      "url": r,
+      "query": q,
+      "callback": onFulfilled,
+    });
+    processAsyncRequests();
+  });
 }
 
 function asyncQuery(q: string): Promise {
-	return asyncRequest('query', q);
+  return asyncRequest('query', q);
 }
 
 function asyncQueryAndUndo(q: string): Promise {
-	return asyncRequest('queryundo', q);
+  return asyncRequest('queryundo', q);
 }
 
 function asyncUndo(): Promise {
-	return asyncRequest('undo', '');
+  return asyncRequest('undo', '');
 }
 
 function asyncRevision(): Promise {
-	return asyncRequest('revision', '');
+  return asyncRequest('revision', '');
 }
 
 function asyncRewind(delta: number): Promise {
-	console.log('Rewind', delta);
-	return asyncRequest('rewind', delta.toString());
+  console.log('Rewind', delta);
+  return asyncRequest('rewind', delta.toString());
 }
 
 function asyncLog(s: string): Promise {
-	var time = "[" + new Date().toLocaleString() + "] ";
-	return asyncRequest("log", time + s);
+  var time = "[" + new Date().toLocaleString() + "] ";
+  return asyncRequest("log", time + s);
 }
 
 function asyncStatus(): Promise {
-	return asyncRequest("status", "").then(function(response) {
-		var msg = response.contents[0];
-		var r = msg.match("^\\\((.*),(.*),(.*),\"(.*)\",\"(.*)\"\\\)$");
-		var result = {
-			"sections": r[1],
-			"current": (r[2] === "Nothing") ? null : r[2].substring(5).replace(
-				/"/g, ""),
-			"currents": r[3],
-			"label": +r[4],
-			"proving": (r[5] === "1"),
-			"response": response,
-		};
-		return result;
-	});
+  return asyncRequest("status", "").then(function(response) {
+    var msg = response.contents[0];
+    var r = msg.match("^\\\((.*),(.*),(.*),\"(.*)\",\"(.*)\"\\\)$");
+    var result = {
+      "sections": r[1],
+      "current": (r[2] === "Nothing") ? null : r[2].substring(5).replace(
+        /"/g, ""),
+      "currents": r[3],
+      "label": +r[4],
+      "proving": (r[5] === "1"),
+      "response": response,
+    };
+    return result;
+  });
 }
 
 function asyncCurrentLabel(): Promise {
-	return asyncStatus()
-		.then(function(status) {
-			return status.label;
-		});
+  return asyncStatus()
+    .then(function(status) {
+      return status.label;
+    });
 }
 
 function asyncResetCoq(): Promise {
-	return asyncCurrentLabel()
-		.then(function(label) {
-			if (label > 0) {
-				return asyncRewind(label - 1);
-			} else {
-				return Promise.resolve();
-			}
-		});
+  return asyncCurrentLabel()
+    .then(function(label) {
+      if (label > 0) {
+        return asyncRewind(label - 1);
+      } else {
+        return Promise.resolve();
+      }
+    });
 }
 
 module.exports = {
-	"asyncQuery": asyncQuery,
-	"asyncQueryAndUndo": asyncQueryAndUndo,
-	"asyncUndo": asyncUndo,
-	"asyncRevision": asyncRevision,
-	"asyncRewind": asyncRewind,
-	"asyncLog": asyncLog,
-	"asyncStatus": asyncStatus,
-	"asyncCurrentLabel": asyncCurrentLabel,
-	"asyncResetCoq": asyncResetCoq,
+  "asyncQuery": asyncQuery,
+  "asyncQueryAndUndo": asyncQueryAndUndo,
+  "asyncUndo": asyncUndo,
+  "asyncRevision": asyncRevision,
+  "asyncRewind": asyncRewind,
+  "asyncLog": asyncLog,
+  "asyncStatus": asyncStatus,
+  "asyncCurrentLabel": asyncCurrentLabel,
+  "asyncResetCoq": asyncResetCoq,
 }
--- web/src/lecture.js
+++ web/src/lecture.js
@@ -14,167 +14,167 @@ var focusedOnEditor: bool = true;
 
 $(document).ready(function() {
 
-  $(window).bind('beforeunload', function() {
-    return 'WARNING: unsaved progress wil be lost';
-  });
+	$(window).bind('beforeunload', function() {
+		return 'WARNING: unsaved progress wil be lost';
+	});
 
-  var buttonGroup = $(".btn-group");
+	var buttonGroup = $(".btn-group");
 
-  $("<button>", {
-      "class": "btn btn-default",
-      "id": "prover-down",
-    })
-    .appendTo(buttonGroup)
-    .on("click", function() {
-      Editor.onCtrlDown(true);
-    })
-    .append(mkGlyph("arrow-down"));
+	$("<button>", {
+			"class": "btn btn-default",
+			"id": "prover-down",
+		})
+		.appendTo(buttonGroup)
+		.on("click", function() {
+			Editor.onCtrlDown(true);
+		})
+		.append(mkGlyph("arrow-down"));
 
-  $("<button>", {
-      "class": "btn btn-default",
-      "id": "prover-up",
-    })
-    .appendTo(buttonGroup)
-    .on("click", function() {
-      Editor.onCtrlUp(true);
-    })
-    .append(mkGlyph("arrow-up"))
-    .append(nbsp + "Prev");
+	$("<button>", {
+			"class": "btn btn-default",
+			"id": "prover-up",
+		})
+		.appendTo(buttonGroup)
+		.on("click", function() {
+			Editor.onCtrlUp(true);
+		})
+		.append(mkGlyph("arrow-up"))
+		.append(nbsp + "Prev");
 
-  $("<button>", {
-      "class": "btn btn-default",
-      "id": "prover-caret",
-    })
-    .appendTo(buttonGroup)
-    .on("click", function() {
-      Editor.onCtrlEnter();
-    })
-    .append(mkGlyph("arrow-right"))
-    .append(mkGlyph("italic"))
-    .append(nbsp + "Go to caret");
+	$("<button>", {
+			"class": "btn btn-default",
+			"id": "prover-caret",
+		})
+		.appendTo(buttonGroup)
+		.on("click", function() {
+			Editor.onCtrlEnter();
+		})
+		.append(mkGlyph("arrow-right"))
+		.append(mkGlyph("italic"))
+		.append(nbsp + "Go to caret");
 
-  $("<button>", {
-      "class": "btn btn-default",
-      "html": $("<span>")
-        .append(mkGlyph("edit"))
-        .append(nbsp + "View Editor"),
-      "id": "peek-button",
-    })
-    .appendTo(buttonGroup)
-    .css("display", "none")
-    .on("click", focusEditorUI);
+	$("<button>", {
+			"class": "btn btn-default",
+			"html": $("<span>")
+				.append(mkGlyph("edit"))
+				.append(nbsp + "View Editor"),
+			"id": "peek-button",
+		})
+		.appendTo(buttonGroup)
+		.css("display", "none")
+		.on("click", focusEditorUI);
 
-  $("<button>", {
-      "class": "btn btn-default",
-      "html": $("<span>")
-        .append(mkGlyph("tree-deciduous"))
-        .append(nbsp + "View Proof Tree"),
-      "id": "unpeek-button",
-    })
-    .appendTo(buttonGroup)
-    .css("display", "none")
-    .on("click", focusProofTreeUI);
+	$("<button>", {
+			"class": "btn btn-default",
+			"html": $("<span>")
+				.append(mkGlyph("tree-deciduous"))
+				.append(nbsp + "View Proof Tree"),
+			"id": "unpeek-button",
+		})
+		.appendTo(buttonGroup)
+		.css("display", "none")
+		.on("click", focusProofTreeUI);
 
 });
 
 function mkGlyph(name: string): Element {
-  return $("<i>", {
-    "class": "glyphicon glyphicon-" + name,
-  });
+	return $("<i>", {
+		"class": "glyphicon glyphicon-" + name,
+	});
 }
 
 function onRequestTriggered(requestType: string, request: string) {
-  switch (requestType) {
-    case 'query':
-      break;
-  }
+	switch (requestType) {
+		case 'query':
+			break;
+	}
 }
 
 function resize() {
-  var windowHeight = $(window).height();
-  var width = $(window).width();
-  var height = $("#prooftree").height();
-  var proofTree = Globals.activeProofTree;
-  if (proofTree) {
-    proofTree.resize($(window).width(), height);
-  }
-  resizeCoqtopPanes();
-  Editor.scrollIntoView();
+	var windowHeight = $(window).height();
+	var width = $(window).width();
+	var height = $("#prooftree").height();
+	var proofTree = Globals.activeProofTree;
+	if (proofTree) {
+		proofTree.resize($(window).width(), height);
+	}
+	resizeCoqtopPanes();
+	Editor.scrollIntoView();
 }
 
 function resizeCoqtopPanes() {
 
-  if (focusedOnEditor) {
+	if (focusedOnEditor) {
 
-    var contextLength = Globals.docContext.getValue().length;
-    var responseLength = Globals.docResponse.getValue().length;
+		var contextLength = Globals.docContext.getValue().length;
+		var responseLength = Globals.docResponse.getValue().length;
 
-    if (contextLength === 0 && responseLength === 0) {
-      $("#coqtop-context").height("50%");
-      $("#coqtop-response").height("50%");
-    } else if (contextLength === 0) {
-      $("#coqtop-context").height("0%");
-      $("#coqtop-response").height("100%");
-    } else if (responseLength === 0) {
-      $("#coqtop-context").height("100%");
-      $("#coqtop-response").height("0%");
-    } else {
-      $("#coqtop-context").height("80%");
-      $("#coqtop-response").height("20%");
-    }
+		if (contextLength === 0 && responseLength === 0) {
+			$("#coqtop-context").height("50%");
+			$("#coqtop-response").height("50%");
+		} else if (contextLength === 0) {
+			$("#coqtop-context").height("0%");
+			$("#coqtop-response").height("100%");
+		} else if (responseLength === 0) {
+			$("#coqtop-context").height("100%");
+			$("#coqtop-response").height("0%");
+		} else {
+			$("#coqtop-context").height("80%");
+			$("#coqtop-response").height("20%");
+		}
 
-  } else {
+	} else {
 
-    $("#coqtop-context").height("0%");
-    $("#coqtop-response").height("100%");
+		$("#coqtop-context").height("0%");
+		$("#coqtop-response").height("100%");
 
-  }
+	}
 
 }
 
 function focusEditorUI() {
 
-  focusedOnEditor = true;
+	focusedOnEditor = true;
 
-  $("#main").height("100%");
-  $("#prooftree").height("0%");
+	$("#main").height("100%");
+	$("#prooftree").height("0%");
 
-  $("#peek-button").css("display", "none");
-  $("#unpeek-button").css("display", "");
+	$("#peek-button").css("display", "none");
+	$("#unpeek-button").css("display", "");
 
-  resize();
-  Globals.cm.refresh();
-  Editor.scrollIntoView();
-  Globals.cm.focus();
+	resize();
+	Globals.cm.refresh();
+	Editor.scrollIntoView();
+	Globals.cm.focus();
 
 }
 
 function focusProofTreeUI() {
 
-  focusedOnEditor = false;
+	focusedOnEditor = false;
 
-  $("#main").height("30%");
-  $("#prooftree").height("70%");
+	$("#main").height("30%");
+	$("#prooftree").height("70%");
 
-  $("#peek-button").css("display", "");
-  $("#unpeek-button").css("display", "none");
+	$("#peek-button").css("display", "");
+	$("#unpeek-button").css("display", "none");
 
-  $("#coqtop-context").height("0%");
-  $("#coqtop-response").height("100%");
+	$("#coqtop-context").height("0%");
+	$("#coqtop-response").height("100%");
 
-  resize();
-  Globals.cm.refresh();
-  Editor.scrollIntoView();
-  var activeProofTree = Globals.activeProofTree;
-  if (activeProofTree) {
-    activeProofTree.getFocus();
-    activeProofTree.refreshTactics();
-  }
+	resize();
+	Globals.cm.refresh();
+	Editor.scrollIntoView();
+	var activeProofTree = Globals.activeProofTree;
+	if (activeProofTree) {
+		activeProofTree.getFocus();
+		activeProofTree.refreshTactics();
+	}
 
 }
 
 module.exports = {
-  "focusEditorUI": focusEditorUI,
-  "focusProofTreeUI": focusProofTreeUI,
-  "onRequestTriggered": onRequestTriggered,
+	"focusEditorUI": focusEditorUI,
+	"focusProofTreeUI": focusProofTreeUI,
+	"onRequestTriggered": onRequestTriggered,
 };
