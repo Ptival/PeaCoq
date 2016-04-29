47ae97da18c025a52d32e9ed5d8111d33361597c
Valentin Robert <valentin.robert.42@gmail.com>
2016-04-29 00:51:35 -0700

    added cabal-install and ghc to nix shell

--- shell.nix
+++ shell.nix
@@ -3,7 +3,9 @@ nixpkgs.lib.overrideDerivation
   (import ./default.nix { inherit nixpkgs compiler; }).env
   (old:
     { buildInputs = old.buildInputs ++ (with nixpkgs; [
+        cabal-install
         coq_8_5
+        ghc
         ocaml_4_02
         ocamlPackages.camlp5_6_strict
         nodejs-5_x
