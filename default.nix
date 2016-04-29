72db7222c3cd49bfba823868f02f4bf2b502fbd4
Valentin Robert <valentin.robert.42@gmail.com>
2016-04-28 20:25:48 -0700

    fixed the nix files, about ready to make a making script

--- default.nix
+++ default.nix
@@ -1,13 +1,2 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7103" }:
-let drv = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./peacoq.nix {}; in
-drv.overrideDerivation (
-  attrs:
-    with nixpkgs;
-    { propagatedBuildInputs =
-        attrs.propagatedBuildInputs ++ [
-          coq_8_5
-          ocaml
-          ocamlPackages.camlp5_6_strict
-        ];
-    }
-)
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./peacoq.nix {}
--- shell.nix
+++ shell.nix
@@ -1,2 +1,12 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7103" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+nixpkgs.lib.overrideDerivation
+  (import ./default.nix { inherit nixpkgs compiler; }).env
+  (old:
+    { buildInputs = old.buildInputs ++ (with nixpkgs; [
+        coq_8_5
+        ocaml_4_02
+        ocamlPackages.camlp5_6_strict
+        nodejs-5_x
+      ]);
+    }
+  )
