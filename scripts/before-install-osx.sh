070e3f4d51a71f63980a96e37d4d96b8daa57979
Valentin Robert <valentin.robert.42@gmail.com>
2016-05-09 14:11:54 -0700

    does it work with correct version numbers or not

--- scripts/before-install-osx.sh
+++ scripts/before-install-osx.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -ev
 
+ALEXVER="3.1.4"
+CABALVER="1.22.9.0"
+GHCVER="7.10.3b"
+HAPPYVER="1.19.5"
 PKGMGR="brew"
 
 $PKGMGR update
-$PKGMGR install cabal-install
+$PKGMGR install cabal-install-$CABALVER
 $PKGMGR install ghc-$GHCVER
 export PATH=/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:$PATH
 cabal update
