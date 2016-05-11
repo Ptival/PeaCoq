0655f2424eacc62e4ad215b5580ad2801c56e31f
Valentin Robert <valentin.robert.42@gmail.com>
2016-05-11 14:45:17 -0700

    well, osx will also need to build its coq

--- scripts/before-install-osx.sh
+++ scripts/before-install-osx.sh
@@ -14,3 +14,12 @@ cabal install alex happy
 
 # Coq and OCaml plugin
 $PKGMGR install coq ocaml camlp5
+# Brew packages a version of coq that was not built with its version of
+# ocaml so coq complains
+wget https://coq.inria.fr/distrib/V8.5pl1/files/coq-8.5pl1.tar.gz
+tar -xzvf coq-8.5pl1.tar.gz
+( cd coq-8.5pl1
+  yes "" | ./configure
+  make
+  sudo make install
+) || exit 1
