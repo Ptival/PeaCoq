ad569a84e2599e31391da8cd3f7dc8a4403818f9
Valentin Robert <valentin.robert.42@gmail.com>
2016-05-12 02:01:41 -0700

    fix linux build

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ cabal --version >/dev/null 2>&1 || missing "cabal-install"
 camlp5 -v       >/dev/null 2>&1 || missing "camlp5"
 coqc -v         >/dev/null 2>&1 || missing "coq"
 if [[ `coqtop -v` != *"version 8.5"* ]]; then missing "coqtop version 8.5"; fi
+ghc --version   >/dev/null 2>&1 || missing "ghc"
 ocamlc -v       >/dev/null 2>&1 || missing "ocaml"
 
 if [ -n "${NIX_LDFLAGS+x}" ] && [ -n "${NIX_CFLAGS_COMPILE+x}" ]; then
