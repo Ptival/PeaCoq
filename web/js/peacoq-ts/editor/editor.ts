473a441c9711be3ecca6d6104f20fa792ae457e0
Valentin Robert <valentin.robert.42@gmail.com>
2016-05-26 15:33:41 -0700

    fixed bad display of goals

--- web/js/peacoq-ts/editor/editor.ts
+++ web/js/peacoq-ts/editor/editor.ts
@@ -28,21 +28,25 @@ export function displayEdit(c: PeaCoqContext): void {
     Global.tabs.pretty.click();
     // TODO: if performance becomes an issue, do this more lazily?
     Global.tabs.foreground.setValue(
-      _(c.fgGoals).map((g) => g.toString()).value().join("\n\n\n"), false
+      _(c.fgGoals).map(g => g.goal.toString()).value().join("\n\n\n"), false
     );
     Global.tabs.foreground.setCaptionSuffix("(" + c.fgGoals.length + ")");
     const bgGoals = _(c.bgGoals).map((ba) => [].concat(ba.before, ba.after)).flatten().value();
     Global.tabs.background.setValue(
-      _(bgGoals).map((g) => g.toString()).value().join("\n\n\n"), false
+      _(c.bgGoals).map(ba =>
+        _(ba.before).map(g => g.goal.toString()).value().join("\n\n\n")
+        +
+        _(ba.after).map(g => g.goal.toString()).value().join("\n\n\n")
+      ).value().join("\n\n\n"), false
     );
     const nbBgGoals = countBackgroundGoals(c);
     Global.tabs.background.setCaptionSuffix("(" + nbBgGoals + ")");
     Global.tabs.shelved.setValue(
-      _(c.shelvedGoals).map((g) => g.toString()).value().join("\n\n\n"), false
+      _(c.shelvedGoals).map(g => g.goal.toString()).value().join("\n\n\n"), false
     );
     Global.tabs.shelved.setCaptionSuffix("(" + c.shelvedGoals.length + ")");
     Global.tabs.givenUp.setValue(
-      _(c.givenUpGoals).map((g) => g.toString()).value().join("\n\n\n"), false
+      _(c.givenUpGoals).map(g => g.goal.toString()).value().join("\n\n\n"), false
     );
     Global.tabs.givenUp.setCaptionSuffix("(" + c.givenUpGoals.length + ")");
 }
