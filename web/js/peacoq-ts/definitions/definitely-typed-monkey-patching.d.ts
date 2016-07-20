613243b87ecd1e48546b819586b666431bc39590
Valentin Robert <valentin.robert.42@gmail.com>
2016-07-20 11:45:15 -0700

    monkey-patched TypeScript definition for Rx.Observable.prototype.last()

--- web/js/peacoq-ts/definitions/definitely-typed-monkey-patching.d.ts
+++ web/js/peacoq-ts/definitions/definitely-typed-monkey-patching.d.ts
@@ -49,17 +49,13 @@ interface JQueryStatic {
 
 declare var sexpParse: (o: any) => any;
 
-// adding filter with cast
-declare namespace Rx {
-  export interface Observable<T> extends IObservable<T> {
-    filter<U>(predicate: (value: T, index: number, source: Observable<T>) => boolean, thisArg?: any): Observable<U>;
-  }
-}
-
 // adding delay with selector
 declare namespace Rx {
   export interface Observable<T> {
     delay(subscriptionDelay: Observable<any>, delayDurationSelector: (t: T) => Rx.Observable<any>): Observable<T>;
+    // a version of filter that lets me cast the type (when filtering with type assertion)
+    filter<U>(predicate: (value: T, index: number, source: Observable<T>) => boolean, thisArg?: any): Observable<U>;
+    last(): Observable<T>;
   }
 }
 
