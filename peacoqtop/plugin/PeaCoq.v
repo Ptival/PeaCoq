a29670ebfd81321611c48a266b8958b58b6141c7
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-01 11:55:15 -0700

    disabling the notation hack for now

--- peacoqtop/plugin/PeaCoq.v
+++ peacoqtop/plugin/PeaCoq.v
@@ -1,9 +1,12 @@
 Declare ML Module "peacoq_utils".
 Declare ML Module "peacoq_plugin".
 
+(*
 Notation
   "'<peacoq-plus><term>' x '</term><term>' y '</term></peacoq-plus>'"
   := (plus x y) (at level 50)
   : peacoq_scope.
 
 Open Scope peacoq_scope.
+*)
+
