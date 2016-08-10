e18535f73c772739860fd458c8695fb3493a4af3
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-09 18:40:52 -0700

    what about this?

--- scripts/before-install-linux.sh
+++ scripts/before-install-linux.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euv
 
+OPAMINSTALLER="https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh"
+wget $OPAMINSTALLER -O - | sh -s /usr/local/bin
+
 (
 git clone https://github.com/coq/coq.git
 cd coq
