ca9c34f9cc505c091754b0af45115b119255d9bc
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-11 15:03:54 -0700

    travis

--- scripts/before-install-coq-serapi.sh
+++ scripts/before-install-coq-serapi.sh
@@ -5,7 +5,7 @@ if [ ! -f coq/.git/config ]; then
   git clone https://github.com/ejgallego/coq-serapi.git
 fi
 cd coq-serapi
-ls
+ls -lah
 git pull
 # Holy shit, sed on OSX and on Linux are really hard to make work the same...
 sed -i.bak "s|/home/egallego/external/coq-git/|$TRAVIS_BUILD_DIR/coq/|g" myocamlbuild.ml
