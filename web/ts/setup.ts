fab6f0836799aa19557f29b3ed6d9523b7eda6a2
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-22 15:11:45 -0700

    bugfix: cancels caused by PeaCoq were improperly computed since recent change

--- web/ts/setup.ts
+++ web/ts/setup.ts
@@ -498,22 +498,22 @@ $(document).ready(() => {
 
   const stmCanceledFiltered$ = new Rx.Subject<ISertop.IAnswer<ISertop.IStmCanceled>>();
 
-  coqtopOutput$s.answer$s.stmCanceled$.subscribe(a => {
-
-    const removedStateIds = a.answer.stateIds;
-
-    // if this looks like a cancel caused by PeaCoq trying tactics, ignore it
-    if (!_.some(removedStateIds, sid => isJust(doc.getSentenceByStateId(sid)))) {
-      return;
-    }
-
-    stmCanceledFiltered$.onNext(a);
-
-    doc.removeSentencesByStateIds(removedStateIds);
-    const tip = _.maxBy(doc.getAllSentences(), s => s.sentenceId);
-    if (tip) { doc.setTip(tip); }
-
-  });
+  // Now that we pre-emptively removed sentences from the view before they are
+  // acknowledged by the backend, checking which StmCanceled were caused by
+  // PeaCoq's automation is more complex than checking if the removed stateIds
+  // match a sentence in the document.
+  stmCancel$
+    .filter(c => !c.controlCommand.fromAutomation)
+    .flatMap(c =>
+      coqtopOutput$s.answer$s.stmCanceled$.filter(e => e.cmdTag === c.tag)
+    )
+    .subscribe(a => {
+      const removedStateIds = a.answer.stateIds;
+      stmCanceledFiltered$.onNext(a);
+      doc.removeSentencesByStateIds(removedStateIds);
+      const tip = _.maxBy(doc.getAllSentences(), s => s.sentenceId);
+      if (tip) { doc.setTip(tip); }
+    });
 
   // NOTE: CoqExn is pretty useless in indicating which command failed
   // Feedback.ErrorMsg gives the failed state ID
