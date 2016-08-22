90562447f3a873ec9121c1783844a45612c35dea
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-22 15:05:53 -0700

    tests: moving stuff around

--- web/coq/tests/band.v
+++
@@ -1,9 +0,0 @@
-
-Definition band b1 b2 : bool :=
-  match b1 with
-    | true => match b2 with
-                | true => true
-                | false => false
-              end
-    | false => false
-  end.
