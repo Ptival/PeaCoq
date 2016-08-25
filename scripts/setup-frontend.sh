57616620e0040ffaa183b27db26a015ebc8fa0a4
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-25 15:44:35 -0700

    script needed fix after recent changes

--- scripts/setup-frontend.sh
+++ scripts/setup-frontend.sh
@@ -11,10 +11,8 @@ cd node_modules
 ./requirejs/bin/r.js -convert s-expression s-expression-amd
 )
 
-cd ts
-
 ./typings-bin prune
 ./typings-bin install
 
-rm -rf ../js-of-ts
+rm -rf ./js-of-ts
 ./tsc -p . --diagnostics
