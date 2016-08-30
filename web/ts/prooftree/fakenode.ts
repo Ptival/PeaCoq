0335753f2b84d15bbace5828d273c4a781c64332
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-30 12:04:42 -0700

    changed fake node handling

--- web/ts/prooftree/fakenode.ts
+++ web/ts/prooftree/fakenode.ts
@@ -1,18 +1,23 @@
 import { ProofTreeNode } from "./prooftreenode";
 
+function fake(f: FakeNode): any {
+  debugger; throw f;
+}
+
 export class FakeNode extends ProofTreeNode implements IFakeNode {
   constructor(p: IProofTree, parent: IProofTreeNode) {
     super(p, just(parent));
   }
 
-  click(): void { throw "FakeNode"; }
-  getAllDescendants(): IProofTreeNode[] { throw "FakeNode"; }
-  getAllGoalDescendants(): IGoalNode[] { throw "FakeNode"; }
-  getFocusedChild(): Maybe<IProofTreeNode> { throw "FakeNode"; }
-  getGoalAncestor(): Maybe<IGoalNode> { throw "FakeNode"; }
-  getHeight(): number { throw "FakeNode"; }
-  getParent(): Maybe<IProofTreeNode> { throw "FakeNode"; }
-  getViewChildren(): IProofTreeNode[] { throw "FakeNode"; }
-  getWidth(): number { throw "FakeNode"; }
-  isSolved(): boolean { throw "FakeNode"; }
+  click(): void { return fake(this); }
+  focus(): void { return fake(this); }
+  getAllDescendants(): IProofTreeNode[] { return fake(this); }
+  getAllGoalDescendants(): IGoalNode[] { return fake(this); }
+  getFocusedChild(): Maybe<IProofTreeNode> { return fake(this); }
+  getGoalAncestor(): Maybe<IGoalNode> { return fake(this); }
+  getHeight(): number { return fake(this); }
+  getParent(): Maybe<IProofTreeNode> { return fake(this); }
+  getViewChildren(): IProofTreeNode[] { return fake(this); }
+  getWidth(): number { return fake(this); }
+  isSolved(): boolean { return fake(this); }
 }
