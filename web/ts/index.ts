3f0e2df2ed56bef170d1d4c8c35d5fedf883df78
Valentin Robert <valentin.robert.42@gmail.com>
2016-09-06 17:27:51 -0700

    started factoring out layout

--- web/ts/index.ts
+++ web/ts/index.ts
@@ -8,6 +8,7 @@ import * as DisplayContext from "./editor/display-context";
 import * as Editor from "./editor/editor";
 import { isBefore } from "./editor/editor-utils";
 import { setupKeybindings } from "./editor/keybindings";
+import * as Layout from "./editor/layout";
 import { setupProgressBar } from "./editor/progress-bar";
 import * as SentenceToDisplay from "./editor/sentence-to-display";
 import * as Stage from "./editor/stage";
@@ -41,30 +42,45 @@ import * as QueryCommand from "./sertop/query-command";
 let fontSize = 16; // pixels
 const resizeBufferingTime = 250; // milliseconds
 
-// TODO: this should not be global
-let layout: W2UI.W2Layout;
-let bottomLayout: W2UI.W2Layout;
-// const rightLayout: W2UI.W2Layout;
-// const contextTabs: W2UI.W2Tabs;
-// const coqtopTabs: W2UI.W2Tabs;
-
 const peaCoqGetContextRouteId = 1;
 
 $(document).ready(() => {
 
-  $("#interface").w2layout({
-    name: "layout",
-    panels: [
-      { type: "top", size: 34, resizable: false, content: $("<div>", { id: "toolbar" }) },
-      { type: "left", size: "50%", overflow: "hidden", resizable: true, content: $("<div>", { id: "editor", style: "height: 100%" }) },
-      { type: "main", size: "50%", overflow: "hidden", content: $("<div>", { id: "right" }) },
-      { type: "bottom", size: "20px", overflow: "hidden", resizable: false, content: $("<div>", { id: "bottom" }) },
-    ]
-  });
+  const {
+    bottomLayout,
+    contextTabs,
+    layoutResizeStream,
+    rightLayout,
+    rightLayoutRenderedStream,
+    rightLayoutResizeStream,
+    windowResizeStream,
+  } = Layout.setup();
+
+  const resize$ =
+    Rx.Observable.merge(windowResizeStream, layoutResizeStream, rightLayoutResizeStream)
+      // only fire once every <resizeBufferingTime> milliseconds
+      .bufferWithTime(resizeBufferingTime).filter(a => !_.isEmpty(a));
 
   const editor = ace.edit("editor");
 
   const doc: ICoqDocument = new CoqDocument(editor);
+
+  bottomLayout.on({ type: "render", execute: "after" }, () => {
+    setupProgressBar(doc);
+    bottomLayout.refresh();
+  });
+
+  const tabsAreReadyPromise = new Promise(onFulfilled => {
+    rightLayoutRenderedStream.take(1).subscribe(() => {
+      const tabs: ITabs = <any>{};
+      // top panes
+      contextTabs.click("pretty");
+      doc.contextPanel = new ContextPanel(doc, Layout.rightLayoutName);
+      // TODO: stream this
+      updateFontSize(doc);
+      onFulfilled();
+    });
+  });
 
   const sentenceToDisplay$ = SentenceToDisplay.setup(doc);
   const stmObserve$ = DisplayContext.setup(doc, sentenceToDisplay$);
@@ -99,74 +115,6 @@ $(document).ready(() => {
   Editor.setupMainEditor(doc, editor);
 
   editor.focus();
-
-  const rightLayoutName = "right-layout";
-  const bottomLayoutName = "bottom-layout";
-
-  $().w2layout({
-    name: rightLayoutName,
-    panels: [
-      { type: "main", size: "50%", resizable: true, tabs: { tabs: [], } },
-      { type: "bottom", size: "50%", resizable: true, content: $("<div>", { id: "bottom-right" }) },
-    ],
-  });
-
-  $().w2layout({
-    name: bottomLayoutName,
-    panels: [
-      { type: "top", size: "90%", overflow: "hidden", hidden: true, resizable: false, content: $("<div>", { id: "prooftree" }) },
-      { type: "main", size: "10%", overflow: "hidden", resizable: false, content: $("<div>", { id: "progress-bar", height: "100%" }) },
-    ],
-  });
-
-  layout = w2ui["layout"];
-  const rightLayout = w2ui[rightLayoutName];
-  bottomLayout = w2ui[bottomLayoutName];
-  const contextTabs = w2ui[rightLayoutName + "_main_tabs"];
-  contextTabs.onClick = function(event: W2UI.W2Event) { $("#myTabsContent").html(event.target); };
-  const coqtopTabs = w2ui[rightLayoutName + "_bottom_tabs"];
-
-  bottomLayout.on({ type: "render", execute: "after" }, () => {
-    setupProgressBar(doc);
-    bottomLayout.refresh();
-  });
-
-  const rightLayoutRenderedStream = Rx.Observable
-    .create(observer => {
-      rightLayout.on({ type: "render", execute: "after" }, () => observer.onNext({}));
-    })
-    .share();
-
-  const tabsAreReadyPromise = new Promise(onFulfilled => {
-    rightLayoutRenderedStream.take(1).subscribe(() => {
-
-      const tabs: ITabs = <any>{};
-
-      // top panes
-
-      contextTabs.click("pretty");
-
-      doc.contextPanel = new ContextPanel(doc, rightLayoutName);
-
-      // TODO: stream this
-      updateFontSize(doc);
-
-      onFulfilled();
-    });
-
-  });
-
-  layout.content("main", rightLayout);
-  layout.content("bottom", bottomLayout);
-
-  const windowResizeStream: Rx.Observable<{}> = Rx.Observable.fromEvent($(window), "resize");
-  const layoutResizeStream = setupW2LayoutResizeStream(layout);
-  const rightLayoutResizeStream = setupW2LayoutResizeStream(rightLayout);
-
-  const resize$ =
-    Rx.Observable.merge(windowResizeStream, layoutResizeStream, rightLayoutResizeStream)
-      // only fire once every <resizeBufferingTime> milliseconds
-      .bufferWithTime(resizeBufferingTime).filter(a => !_.isEmpty(a));
 
   resize$.subscribe(() => onResize(doc));
   resize$.subscribe(() => doc.contextPanel.onResize());
@@ -539,7 +487,7 @@ $(document).ready(() => {
 
   // keep this above the subscription that removes edits
   new CoqtopPanel(
-    $(w2ui[rightLayoutName].get("bottom").content),
+    $(w2ui[Layout.rightLayoutName].get("bottom").content),
     Rx.Observable.merge(
 
       coqtopOutput$s.feedback$s.message$s.error$
@@ -691,12 +639,12 @@ $(document).ready(() => {
   ProofTreeSetup.setup({
     doc,
     cancelSubject,
-    hideProofTreePanel,
+    hideProofTreePanel: () => hideProofTreePanel(bottomLayout),
     loadedFile$: userActionStreams.loadedFile$,
     nextSubject,
     resize$,
     sentenceProcessed$: doc.sentenceProcessed$,
-    showProofTreePanel,
+    showProofTreePanel: () => showProofTreePanel(bottomLayout),
     stmCanceled$: stmCanceledFiltered$,
   });
 
@@ -720,22 +668,22 @@ export function onResize(doc: ICoqDocument): void {
 }
 
 // TODO: use an AsyncSubject instead?
-function showProofTreePanel(): Promise<{}> {
+function showProofTreePanel(bottomLayout: W2UI.W2Layout): Promise<{}> {
   return new Promise(function(onFulfilled) {
     const handler = function handler(event: W2UI.W2Event) {
       event.onComplete = onFulfilled;
       bottomLayout.off("show", handler);
     };
     bottomLayout.on("show", handler);
-    layout.set("bottom", { size: "30%" });
-    bottomLayout.show("top");
+    // layout.set("bottom", { size: "60%" });
+    bottomLayout.show("main");
   });
 }
 
-function hideProofTreePanel(): void {
+function hideProofTreePanel(bottomLayout: W2UI.W2Layout): void {
   // debugger;
-  layout.set("bottom", { size: "20px" });
-  bottomLayout.hide("top");
+  // layout.set("bottom", { size: "20px" });
+  bottomLayout.hide("main");
 }
 
 function updateFontSize(doc: ICoqDocument): void {
@@ -744,15 +692,6 @@ function updateFontSize(doc: ICoqDocument): void {
   jss.set("#pretty-content", { "font-size": fontSize + "px" });
   jss.set("svg body", { "font-size": fontSize + "px" });
   doc.getActiveProofTree().fmap(t => t.scheduleUpdate());
-}
-
-function setupW2LayoutResizeStream(layout: W2UI.W2Layout): Rx.Observable<{}> {
-  return Rx.Observable
-    .create(observer => {
-      layout.on({ type: "resize", execute: "after" }, () => observer.onNext({}));
-    })
-    .share()
-    ;
 }
 
 interface UserActionStreams {
