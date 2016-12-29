9434d45a6a780a03bd63f8f3ff541e2f5d0224b6
Valentin Robert <valentin.robert.42@gmail.com>
2016-12-29 09:17:08 -0800

    stop having local Snap stuff

--- shell.nix
+++ shell.nix
@@ -1,13 +1,13 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc801" }:
 let callPackage = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage; in
-let snapPath = ./peacoq-server/snap-framework; in
-let xmlhtml = callPackage "${snapPath}/xmlhtml/xmlhtml.nix" {}; in
-let heist = callPackage "${snapPath}/heist/heist.nix" { inherit xmlhtml; }; in
-let io-streams-haproxy = callPackage "${snapPath}/io-streams-haproxy/io-streams-haproxy.nix" {}; in
-let snap-core = callPackage "${snapPath}/snap-core/snap-core.nix" {}; in
-let snap-server = callPackage "${snapPath}/snap-server/snap-server.nix" { inherit io-streams-haproxy snap-core; }; in
-let snap = callPackage "${snapPath}/snap/snap.nix" { inherit heist snap-core snap-server; }; in
-let peacoq-server = callPackage peacoq-server/peacoq-server.nix { inherit snap; }; in
+#let snapPath = ./peacoq-server/snap-framework; in
+#let xmlhtml = callPackage "${snapPath}/xmlhtml/xmlhtml.nix" {}; in
+#let heist = callPackage "${snapPath}/heist/heist.nix" { inherit xmlhtml; }; in
+#let io-streams-haproxy = callPackage "${snapPath}/io-streams-haproxy/io-streams-haproxy.nix" {}; in
+#let snap-core = callPackage "${snapPath}/snap-core/snap-core.nix" {}; in
+#let snap-server = callPackage "${snapPath}/snap-server/snap-server.nix" { inherit io-streams-haproxy snap-core; }; in
+#let snap = callPackage "${snapPath}/snap/snap.nix" { inherit heist snap-core snap-server; }; in
+let peacoq-server = callPackage peacoq-server/peacoq-server.nix {}; in
 nixpkgs.stdenv.mkDerivation {
   name = "peacoq";
   jailbreak = true;
