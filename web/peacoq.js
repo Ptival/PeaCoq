905ee2e2a2eca29df0fea2dfd60ff6457439cbd8
Valentin Robert <valentin.robert.42@gmail.com>
2017-02-13 11:58:24 -0800

    added codemirror and timing

--- web/peacoq.js
+++ web/peacoq.js
@@ -5,6 +5,7 @@ requirejs.config({
     "ace": `${nodePath}/ace-code-editor/lib/ace`,
     "bluebird": `${nodePath}/bluebird/js/browser/bluebird`,
     "bootstrap": `${nodePath}/bootstrap/dist/js/bootstrap`,
+    "codemirror": `${nodePath}/codemirror/lib/codemirror`,
     "d3-array": `${nodePath}/d3-array/build/d3-array`,
     "d3-collection": `${nodePath}/d3-collection/build/d3-collection`,
     "d3-color": `${nodePath}/d3-color/build/d3-color`,
@@ -48,81 +49,96 @@ requirejs.config({
 });
 
 // Start the main app logic.
+const startRequire = Date.now()
 
 // first, the ones that need binding
 requirejs(
   [
-    "ace/ace",
-    "bluebird",
-    "s-expression",
-    "jquery",
+    // "ace/ace", // ~ 500ms
+    // "bluebird", // ~ 100ms
+    "codemirror", // ~100ms
+    // "s-expression", // ~ 30ms
+    // "jquery", // ~ 100ms
   ],
-  (ace, bluebird, sexpParse) => {
-    window.ace = ace;
+  (
+    // ace,
+    bluebird,
+    sexpParse
+  ) => {
+    const pass1 = Date.now()
+    console.log(`Required 1st pass: ${pass1 - startRequire}ms`)
+    // window.ace = ace;
     window.Promise = bluebird;
     // not sure how else this is usually done
-    aceRequires = [
-      ace.require("ace/anchor"),
-      ace.require("ace/range"),
-    ];
-    window.AceAjax = $.extend({}, ...aceRequires);
+    // aceRequires = [
+    //   ace.require("ace/anchor"),
+    //   ace.require("ace/range"),
+    // ];
+    // window.AceAjax = $.extend({}, ...aceRequires);
     window.sexpParse = sexpParse;
     requirejs(
       [
-        "bootstrap",
-        "jquery.hotkeys",
-        "jquery-contextmenu",
+        "bootstrap", // ~50ms
+        "jquery.hotkeys", // ~20ms
+        "jquery-contextmenu", // ~40ms
         // "jquery-ui",
-        "jss",
-        "lodash",
-        "MathJax",
+        "jss", // ~ 20ms
+        "lodash", // ~60ms
+        "MathJax", // ~40ms
         // "polymer-ts",
-        "rx",
-        "tsmonad",
+        "rx", // ~60ms
+        "tsmonad", // ~100ms
         // "webcomponents",
-        "w2ui",
+        "w2ui", // ~80ms
       ],
-      () => require(
-        [
-          "ace/ext/language_tools",
-          "ace/mode/ocaml",
-          "js/highlight-coq",
-          "js/mode-coq",
-          // Unfortunately, at the moment, all these have been written as non-modules
-          "js-of-ts/coq/binder-kind",
-          "js-of-ts/coq/case-style",
-          "js-of-ts/coq/feedback-content",
-          "js-of-ts/coq/local-binder",
-          "js-of-ts/coq/paren-relation",
-          "js-of-ts/coq/prim-token",
-          "js-of-ts/coq/unparsing",
-          "js-of-ts/coq/binding-kind",
-          "js-of-ts/coq/coq-constr-expr",
-          "js-of-ts/coq/feedback",
-          "js-of-ts/coq/message-level",
-          "js-of-ts/coq/ppbox",
-          "js-of-ts/coq/reference",
-          "js-of-ts/coq/value-fail",
-          // "js-of-ts/coq/block-type",
-          "js-of-ts/coq/gallina",
-          // "js-of-ts/coq/message",
-          "js-of-ts/coq/ppcmd-token",
-          "js-of-ts/coq/status",
-          "js-of-ts/coq/xml-tag",
-          "js-of-ts/coq/cases-pattern-expr",
-          "js-of-ts/coq/explicitation",
-          "js-of-ts/coq/glob-sort-gen",
-          "js-of-ts/coq/name-base",
-          "js-of-ts/coq/ppcut",
-          // "js-of-ts/coq/str-token",
-          "js-of-ts/coq/xml-tree",
-          "js-of-ts/peacoq/coq-string-utils",
-          "js-of-ts/peacoq/utils",
-          "js-of-ts/index",
-          // "js-of-ts/ui-container",
-          // "js-of-ts/ui-widget",
-        ]
-      )
+      () => {
+        const pass2 = Date.now()
+        console.log(`Required 2nd pass: ${pass2 - pass1}ms`)
+        require(
+          [
+            "ace/ext/language_tools",
+            "ace/mode/ocaml",
+            "js/highlight-coq",
+            "js/mode-coq",
+            // Unfortunately, at the moment, all these have been written as non-modules
+            "js-of-ts/coq/binder-kind",
+            "js-of-ts/coq/case-style",
+            "js-of-ts/coq/feedback-content",
+            "js-of-ts/coq/local-binder",
+            "js-of-ts/coq/paren-relation",
+            "js-of-ts/coq/prim-token",
+            "js-of-ts/coq/unparsing",
+            "js-of-ts/coq/binding-kind",
+            "js-of-ts/coq/coq-constr-expr",
+            "js-of-ts/coq/feedback",
+            "js-of-ts/coq/message-level",
+            "js-of-ts/coq/ppbox",
+            "js-of-ts/coq/reference",
+            "js-of-ts/coq/value-fail",
+            // "js-of-ts/coq/block-type",
+            "js-of-ts/coq/gallina",
+            // "js-of-ts/coq/message",
+            "js-of-ts/coq/ppcmd-token",
+            "js-of-ts/coq/status",
+            "js-of-ts/coq/xml-tag",
+            "js-of-ts/coq/cases-pattern-expr",
+            "js-of-ts/coq/explicitation",
+            "js-of-ts/coq/glob-sort-gen",
+            "js-of-ts/coq/name-base",
+            "js-of-ts/coq/ppcut",
+            // "js-of-ts/coq/str-token",
+            "js-of-ts/coq/xml-tree",
+            "js-of-ts/peacoq/coq-string-utils",
+            "js-of-ts/peacoq/utils",
+            "js-of-ts/index",
+            // "js-of-ts/ui-container",
+            // "js-of-ts/ui-widget",
+          ],
+          () => {
+            const pass3 = Date.now()
+            console.log(`Required 3rd pass: ${pass3 - pass2}ms`)
+          })
+      }
     );
   }
 );
--- web/ts/index.ts
+++ web/ts/index.ts
@@ -38,15 +38,29 @@ import { setup as setupProofTreeAutomation } from "./prooftree/automation"
 import { setup as setupProofTreePopulating } from "./prooftree/populating"
 import { setup as setupProofTree } from "./prooftree/setup"
 
+
+
 // import * as Promise from 'bluebird'
 // Promise.longStackTraces()
 // Promise.onUnhandledRejectionHandled((reason, promise) => {
 //   debugger
 // })
 
+let lastTimer = Date.now()
+function time(label: string) {
+  const now = Date.now()
+  const delta = now - lastTimer
+  if (delta > 5) {
+    console.log(`Timing ${label}: ${now - lastTimer}ms`)
+  }
+  lastTimer = now
+}
+
 const resizeBufferingTime = 250 // milliseconds
 
 $(document).ready(() => {
+
+  const readyStartTime = Date.now()
 
   const {
     bottomLayout,
@@ -83,80 +97,150 @@ $(document).ready(() => {
     })
   })
 
+  time("start")
   setupDisplayContext(doc)
+  time("A0")
   setupObserveEditorChange(doc)
+  time("B0")
   setupEditor(doc, editor)
+  time("C0")
   editor.focus()
+  time("D0")
   resize$.subscribe(() => onResize(doc))
+  time("E0")
 
-  const toolbar$s = setupToolbar(doc)
+  time("A1")
+
+  // SLOW!
+  // const toolbar$s = setupToolbar(doc)
+  const toolbar$s = {
+    fontDecrease: Rx.Observable.empty(),
+    fontIncrease: Rx.Observable.empty(),
+    goToCaret: Rx.Observable.empty(),
+    load: Rx.Observable.empty(),
+    next: Rx.Observable.empty(),
+    previous: Rx.Observable.empty(),
+    save: Rx.Observable.empty(),
+  }
+
+  time("B1")
   const shortcut$s = setupKeybindings(doc)
+  time("C1")
   const { goTo$, loadedFile$, next$, prev$ } = setupUserActions(doc, toolbar$s, shortcut$s)
+  time("D1")
 
+  time("A2")
   const [forwardGoTo$, backwardGoTo$] = setupGoToPartitioning(doc, goTo$)
+  time("B2")
   setupCancelBecauseBackwardGoTo(doc, backwardGoTo$)
+  time("C2")
   loadedFile$.subscribe(() => doc.contextPanel.clear())
+  time("D2")
   setupQuitBecauseFileLoaded(doc, loadedFile$)
+  time("E2")
   next$.subscribe(() => doc.next())
+  time("F2")
   setupCancelBecausePrev(doc, prev$)
+  time("G2")
 
+  time("A3")
   setupSyntaxHovering()
+  time("B3")
   tabsAreReadyPromise.then(() => Theme.setupTheme(doc))
+  time("C3")
   Theme.afterChange$.subscribe(() => onResize(doc))
+  time("D3")
   // These also help with the initial display...
   Theme.afterChange$.subscribe(() => { rightLayout.refresh() })
+  time("E3")
   Theme.afterChange$.subscribe(() => { bottomLayout.refresh() })
+  time("F3")
 
   // Automated tasks need to stop whenever the user changes the current state
   const stopAutomationRound$: Rx.Observable<{}> = Rx.Observable.empty() // FIXME URGENT
 
-  doc.editor.completers = [{ getCompletions: createGetCompletions(doc, stopAutomationRound$) }]
+  time("F4")
+
+  // This is pretty slow
+  // doc.editor.completers = [{ getCompletions: createGetCompletions(doc, stopAutomationRound$) }]
+
+  time("F5")
 
   // Shorthands for input streams
+  time("F6")
   const controlCommand$ = doc.input$.let(Filters.controlCommand)
+  time("F7")
   const stmAdd$ = controlCommand$.let(Filters.stmAdd)
+  time("F8")
   const stmCancel$ = controlCommand$.let(Filters.stmCancel)
+  time("F9")
   const stmEditAt$ = controlCommand$.let(Filters.stmEditAt)
+  time("F10")
   const stmQuery$ = controlCommand$.let(Filters.stmQuery)
+  time("F11")
 
   // Shorthands for output streams
   const { answer$s, feedback$s } = doc.output$s
+  time("F12")
   const { completed$, coqExn$, stmAdded$, stmCanceled$ } = answer$s
+  time("F13")
   const { error$, notice$ } = feedback$s.message$s
+  time("F14")
   const { processed$ } = feedback$s
+  time("F15")
 
+  time("A4")
   setupObserveContext(doc, notice$, stmQuery$)
+  time("B4")
   const stmActionsInFlightCounter$ = setupInFlightCounter(stmAdd$, stmCancel$, stmEditAt$, completed$)
+  time("C4")
   // setupProofTreeAutomation(completed$, doc, error$, notice$, stmActionsInFlightCounter$, stmAdded$, stopAutomationRound$)
   setupProofTreePopulating(doc, doc.tip$)
+  time("D4")
   setupObserveStmAdded(doc, stmAdded$)
+  time("E4")
   setupUserInteractionForwardGoto(doc, forwardGoTo$, error$)
+  time("F4")
   setupObserveProcessed(doc, processed$)
+  time("G4")
 
+  time("A5")
   setupUnderlineError(doc, error$) // keep this above the subscription that removes edits
+  time("b5")
   const jBottom = $(w2ui[rightLayoutName].get("bottom").content)
+  time("c5")
   setupCoqtopPanel(doc, jBottom, error$, notice$, loadedFile$) // keep this above the subscription that removes edits
+  time("D5")
 
   /***** IMPORTANT:
    * The following lines remove edits from the document, so their subscription
    * must happen after subscriptions that want to inspect the edits before removal.
    * TODO: design it better so that removed edits are streamed out.
    *****/
+  time("A6")
   setupObserveCoqExn(doc, coqExn$, stmAdd$, stmQuery$, completed$)
+  time("B6")
   setupObserveError(doc, error$)
+  time("C6")
 
+  time("A7")
   // debugging
   coqExn$
     .filter(e => e.answer.getMessage().indexOf("Anomaly") >= 0)
     .subscribe(e => { debugger })
+  time("B7")
 
+  time("A8")
   const stmCanceledFiltered$ = setupObserveStmCancel(doc, stmCancel$, stmCanceled$)
+  time("B8")
   setupProofTree(doc, loadedFile$, resize$, stmCanceledFiltered$, bottomLayout)
-
+  time("C8")
   const namesInScope$ = setupNamesInScope(doc, notice$)
-
+  time("D8")
   namesInScope$.subscribe(names => console.log(`${names.length} names in scope`))
+  time("E8")
 
+  time("A9")
   // Debugging:
   doc.editor.setValue(`
     Inductive day : Type :=
@@ -169,5 +253,9 @@ $(document).ready(() => {
     | sunday : day
     .
   `)
+  time("B9")
+
+  const readyStopTime = Date.now()
+  console.log(`Ready handler executed in ${readyStopTime - readyStartTime}ms`)
 
 })
